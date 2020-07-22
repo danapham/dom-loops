693f67449c2adbb7f93dd25f01636e85af4ac002
danapham <danapham12@gmail.com>
2020-07-21 20:01:45 -0500

    played around with loops

--- main.js
+++ main.js
@@ -24,35 +24,35 @@
 // console.log(quoteFinder("is"));
 // // logs 7 because that is where "is" starts in the string
 
-const employee = {
-    firstName: "Dana",
-    lastName: "Pham",
-    role: "student",
-    accountNumber:  "74-65995498",
-    isManager: false,
-    titles: ["HR Coordinator", "Genius"],
-    accolade:  {
-      title: "Employee of the Year",
-      dateEarned: "1/2/2020",
-      expires:  "12/31/2020",
-      isCool: true
-    }
+// const employee = {
+//     firstName: "Dana",
+//     lastName: "Pham",
+//     role: "student",
+//     accountNumber:  "74-65995498",
+//     isManager: false,
+//     titles: ["HR Coordinator", "Genius"],
+//     accolade:  {
+//       title: "Employee of the Year",
+//       dateEarned: "1/2/2020",
+//       expires:  "12/31/2020",
+//       isCool: true
+//     }
 
-}
+// }
 
-console.log(employee.firstName)
-const keyToCheck = "accolade";
+// console.log(employee.firstName)
+// const keyToCheck = "accolade";
 
-console.log(employee[keyToCheck]);
-console.log(employee[keyToCheck].isCool);
+// console.log(employee[keyToCheck]);
+// console.log(employee[keyToCheck].isCool);
 
-const addStatus = (employee) => {
-  if (employee.firstName[0] === 'D') {
-    employee.status = "vip";
-  } else {
-    employee.status = "peasant";
-  }
-}
+// const addStatus = (employee) => {
+//   if (employee.firstName[0] === 'D') {
+//     employee.status = "vip";
+//   } else {
+//     employee.status = "peasant";
+//   }
+// }
 
-addStatus(employee);
-console.log(employee);
+// addStatus(employee);
+// console.log(employee);
